c6cb9df1434a9cc0a096d243d1838be8cb795f54
levah Emmanuel <emmanuelnater@gmail.com>
2024-12-29 16:41:22 +0100

    third commit static files

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,15 @@
 from django. urls import path
 from . import views
 
+app_name = "app"
 urlpatterns = [
      path('', views.index, name='index'),
+     path('about/', views.about, name='about'),
+     path('contact/', views.contact, name='contact'),
+     path('teams/', views.teams, name='teams'),
+     path('services/', views.services, name='services'),
+     path('skills/', views.skills, name='skills'),
+     path('contact/', views.contact, name='contact'),
+     path('hire/', views.hire, name='hire'),
+
 ]
--- app/views.py
+++ app/views.py
@@ -3,3 +3,21 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'core/index.html')
+
+def about(request):
+    return render(request, 'core/about.html')
+
+def services(request):
+    return render(request, 'core/services.html')
+
+def skills(request):
+    return render(request, 'core/skills.html')
+    
+def teams(request):
+    return render(request, 'core/teams.html')
+
+def contact(request):
+    return render(request, 'core/contact.html')
+
+def hire(request):
+    return render(request,'hire.html')
