ec742b96a572b9b439523f20c2da3e015656ce1a
Levah <emmanuelnater@gmail.com>
2025-02-20 13:38:29 +0100

    done

--- app/views.py
+++ app/views.py
@@ -4,20 +4,20 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'core/index.html')
 
-def about(request):
-    return render(request, 'core/about.html')
+# def about(request):
+#     return render(request, 'core/about.html')
 
-def services(request):
-    return render(request, 'core/services.html')
+# def services(request):
+#     return render(request, 'core/services.html')
 
-def skills(request):
+def skill(request):
     return render(request, 'core/skills.html')
     
-def teams(request):
-    return render(request, 'core/teams.html')
+# def teams(request):
+#     return render(request, 'core/teams.html')
 
 def contact(request):
     return render(request, 'core/contact.html')
 
-def hire(request):
-    return render(request,'hire.html')
+# def hire(request):
+#     return render(request,'hire.html')
--- backend/settings.py
+++ backend/settings.py
@@ -85,19 +85,26 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 # import dj_database_url
-
-
-# DATABASES = {
-#     'default': dj_database_url.parse(env('DATABASE_URL'))
-# }
-
+from  decouple import config
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':config("DB_NAME"),
+        'USER' :config("DB_USER"),
+        'PASSWORD' :config("DB_PASSWORD"),
+        'HOST' :config("DB_HOST"),
+        'PORT' :config("DB_PORT"),
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
